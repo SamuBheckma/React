20cde9e7355611e6da459d7b073669894d04d986
Samuel <samuelb.conta@gmail.com>
2024-10-04 10:12:38 -0300

    teste

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello, {subject}!</p>
+        <p>Hello, {subject}!!</p>
       </header>
     </div>
   );
