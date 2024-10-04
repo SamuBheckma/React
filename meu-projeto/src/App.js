0bccce4fbf3aa2222977d05ad23304b2fd5d8ee3
Samuel Bheckma <samuelb.conta@gmail.com>
2024-10-04 08:26:07 -0300

    Video teste

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,24 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  export default App;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+     
+     </div>
     </div>
   );
 }
 
-export default App;
+function Video({video}) {
+  return (
+  <div>
+   <Thumbnail video={video} />
+   <a href={video.url}>
+    <h3>{video.title}</h3>
+    <p>{video.description}</p>
+  </a>
+  <LikeButton video={video} />
+  </div>
+);
+}
